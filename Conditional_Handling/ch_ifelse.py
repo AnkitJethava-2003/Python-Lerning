39d2b50d9c8bc2e9ecfdd63e7ef8168d5476a067
AnkitJethava-2003 <ankitjethava1544@gmail.com>
2024-09-26 09:17:25 +0530

    Learned conditional statement

---
+++ Conditional_Handling/ch_ifelse.py
@@ -0,0 +1,14 @@
+import sys
+
+type=sys.argv[1]
+
+
+if type == "t2.micro":
+    print("it will charge you 2 dollars a day")
+elif type == "t2.medium":
+    print("it will charge you 4 dollars a day")
+elif type == "t2.xlarge":
+    print("It will charge you 8 dollars a day")
+else:
+    print("Please provide a valid instance type")
+
